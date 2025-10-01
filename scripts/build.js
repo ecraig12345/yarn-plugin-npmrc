005b7c4fbf85d06ea26ed3d2c8c501e756beeebe
Elizabeth Craig <elcraig@microsoft.com>
2025-09-30 21:56:28 -0700

    why

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const runCommand = require('./runCommand');
+const os = require('os');
 const paths = require('./paths');
 
 fs.rmSync(paths.dist, { recursive: true, force: true });
@@ -12,6 +13,6 @@ function runBuild(/** @type {boolean} */ minify) {
   // The yarn builder has no way to specify output paths, so manually rename the files...
   // Also fix EOLs on Windows
   runCommand('yarn', ['builder', 'build', 'plugin', ...(minify ? [] : ['--no-minify'])]);
-  const contents = fs.readFileSync(paths.bundleFile, 'utf8').replace(/\r\n/g, '\n');
+  const contents = fs.readFileSync(paths.bundleFile, 'utf8').replace(/\r?\n/g, os.EOL);
   fs.writeFileSync(minify ? paths.minBundle : paths.devBundle, contents);
 }
